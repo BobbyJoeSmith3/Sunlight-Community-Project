1decf091efdee780de261134f3092cb0c55fbfc5
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2014-07-27 17:45:34 -0400

    installed various packages, including Django, to the virtual environment. Couldn't get psycopg2 to download. Updated requirements.txt

---
+++ Ella/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/SeestheEnemy/Code/gitrepos/Sunlight-Community-Project/Ella/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
