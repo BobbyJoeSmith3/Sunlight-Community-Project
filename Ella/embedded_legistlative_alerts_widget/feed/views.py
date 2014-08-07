0730882777dc1a772d744f7ea12d7c3a006d40d2
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2014-08-07 14:13:39 -0400

    Added a function to parse the title of the first entry from an rss feed. Going to connect to an html template.

--- Ella/embedded_legistlative_alerts_widget/feed/views.py
+++ Ella/embedded_legistlative_alerts_widget/feed/views.py
@@ -1,7 +1,21 @@
+# Import requests
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
-#import requests
+import feedparser
 
 # Create your views here.
+
+# Connect to the feed app template
 def index(request):
-	return render(request, 'feed/feed_index.html')
+	# Create a variable collection_data for the getScoutCollectionData function below to getting the data to the html template
+	collection_data = getScoutCollectionData()
+	return render(request, 'feed/feed_index.html', {'collection_data': collection_data})
+
+# Stand alone function that will parse the RSS feed xml data for Title, Description, and Publication Date.
+def getScoutCollectionData():
+	# Link to the RSS feed url *For now we'll use mine
+	rss_url = "feed:https://scout.sunlightfoundation.com/user/53cfcc6a0311036a060003be/native-america.rss"
+	# Parse the rss_url
+	d = feedparser.parse(rss_url)
+	# Display the title of the first post
+	d.entries[0].title
